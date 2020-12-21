cc87b8ac5c556f21344ec8c5f0f16f59a1970f41
Abhijeet Chakravorty <>
2020-12-21 22:09:54 +0530

    updated code for tic tac toe

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,10 @@
+import itertools 
+from itertools import combinations, chain
 pos = [0, 2, 4, 8, 10, 12, 16, 18, 20]
 bars = [1, 3, 9, 11, 17, 19]
 bases = []
+def findsubsets(s, n): 
+        return list(map(set, itertools.combinations(s, n))) 
 def makeBoard():
         data = []
         counter = 1
@@ -8,7 +12,6 @@ def makeBoard():
                 if (i in pos):
                         thisdict = {
                                 "index": i,
-                                "type": "pos",
                                 "value": "  ",
                                 "pos": counter,
                                 "marked": False
@@ -18,7 +21,6 @@ def makeBoard():
                         thisdict = {
                                 "index": i,
                                 "value": "|",
-                                "type": "struct",
                                 "pos": None,
                                 "marked": None
                         }
@@ -27,30 +29,18 @@ def makeBoard():
                         thisdict = {
                                 "index": i,
                                 "value": "___",
-                                "type": "base",
                                 "pos": None,
                                 "marked": None
                         }
                 
                 data.append(thisdict)
         return data
-
 def printBoard(data):
         for i in range(len(data)):
                 if (i == 4 or i == 7 or i == 12 or i == 15):
                         print(data[i]["value"], end="\n")
                 else:
                         print(data[i]["value"], end="")
-
-
-def checkMovementStatus(data):
-        for i in range(len(data)):
-                if i in pos:
-                        if not data[i]["value"]:
-                                return False
-                        else:
-                                return True
-
 def updatePosition(board, position, player):
         for i in range(len(board)):
                 if (board[i]["pos"] == position):
@@ -64,38 +54,39 @@ def updatePosition(board, position, player):
                         else:
                                 print("This item has already been marked")
                                 if (player == 1):
-                                        playerOneMoveFunc(board)
+                                        playerOneMoveFunc(board, player)
                                 else:
-                                        playerTwoMoveFunc(board)
+                                        playerTwoMoveFunc(board, player)
         return board
-
-
-def playerOneMoveFunc(board):
-        playerOneMove = int(input("\n"+player1+" please choose position: "))
+def playerOneMoveFunc(board, player1):
+        playerOneMove = int(input("\n"+str(player1)+" please choose position: "))
         if (playerOneMove < 10):
                 updatePosition(board, playerOneMove, 1)
         else:
                 print("This position does not exist in the board")
-                playerOneMoveFunc(board)
-
-def playerTwoMoveFunc(board):
-        playerTwoMove = int(input("\n"+player2+" please choose position: "))
+                playerOneMoveFunc(board, player1)
+def playerTwoMoveFunc(board, player2):
+        playerTwoMove = int(input("\n"+str(player2)+" please choose position: "))
         if (playerTwoMove < 10):
                 updatePosition(board, playerTwoMove, 2)
         else:
                 print("This position does not exist in the board")
-                playerTwoMoveFunc(board)
-        
-
+                playerTwoMoveFunc(board, player2)
+def checkWinner(board):
+        data = findsubsets(pos, 3)
+        for i in range(len(data)):
+                if (i==0):
+                        print(type(data[i]))
+                
 print("Welcome to Tic Tac Toe!")
 print("\n\n\n")
 board = makeBoard()
-# print(board)
 printBoard(board)
 print("\n\n\n")
 print("\nThere are 2 players in this game. \n Player 1 marks X and player 2 marks O \n Do not cheat. Enjoy!")
 player1 = input("Please provide player 1 name: ")
 player2 = input("Please provide player 2 name: ")
 while(True):
-        playerOneMoveFunc(board)
-        playerTwoMoveFunc(board)
+        playerOneMoveFunc(board, player1)
+        playerTwoMoveFunc(board, player2)
+        checkWinner(board)
